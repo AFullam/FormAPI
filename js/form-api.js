27b66b612031cfe9a28a84108f21d9bf89d9ea7f
Andrew Fullam <andrew.fullam@live.co.uk>
2016-07-14 09:37:06 +0100

    Started form input type code

--- js/form-api.js
+++ js/form-api.js
@@ -1,28 +1,59 @@
 /*global $,console*/
 /*jslint node: true*/
 
-var form;
-var id;
-
-form = {
+var form = {
     id: "",
     form_fields: "",
+    action: "",
     form_init: function (form_details) {
         "use strict";
-        var item;
+        //Setup form vars
         this.id = form_details.id;
+        this.form_fields = form_details.form_fields;
+        this.action = form_details.action;
 
-        for (item in form_details.form_fields) {
-            if (form_details.form_fields.hasOwnProperty(item)) {
-                var type = form_details.form_fields[item].type;
-                var size = form_details.form_fields[item].size;
-                var id = form_details.form_fields[item].name;
+        this.form_create();
+    },
+    form_create: function () {
+        "use strict";
+        var item, input_type, type, maxlength, id, option;
 
-                $("#init-form").append("<p>" + id + ": <input type='" + type +
-                                   "' size=" + size +
-                                   "' name=" + id +
-                                   "' id='" + id +
-                                   "'></input></p>");
+        //Process form fields and add to form
+        for (item in this.form_fields) {
+            if (this.form_fields.hasOwnProperty(item)) {
+                input_type = this.form_fields[item].input_type;
+                type = this.form_fields[item].type;
+                maxlength = this.form_fields[item].maxlength;
+                id = this.form_fields[item].name;
+
+                if (type !== "radio") {
+                    $("#" + this.id).append("<p>" + id + ": <" + input_type + " type='" + type +
+                                       "' maxlength='" + maxlength +
+                                       "' name='" + id +
+                                       "' id='" + id +
+                                       "'></input></p>");
+                }
+
+                //If field is select then define options
+                if (input_type === "select") {
+                    for (option in this.form_fields[item].options) {
+                        if (this.form_fields[item].options.hasOwnProperty(option)) {
+                            $("#" + id).append('"<option value"' + option + '">' + this.form_fields[item].options[option] + '</option>');
+                        }
+                    }
+                } else if (input_type === "input" && type === "radio") {
+                //If field is radio button
+                    for (option in this.form_fields[item].options) {
+                        if (this.form_fields[item].options.hasOwnProperty(option)) {
+                            $("#" + this.id).append('<p>' + this.form_fields[item].options[option] +
+                                                ': <' + input_type + ' type="' + type +
+                                                '" value="' + this.form_fields[item].options[option] +
+                                                '" name="' + id +
+                                                '" id="' + id +
+                                                '"></input></p>');
+                        }
+                    }
+                }
             }
         }
     }
--- js/main-form.js
+++ js/main-form.js
@@ -4,16 +4,34 @@ $(document).ready(function () {
     "use strict";
     var form_details = {
         id: "init-form",
+        action: "/",
         form_fields: {
             input: {
+                input_type: "input",
                 type: "text",
-                size: 20,
+                maxlength: 20,
                 name: "Name"
             },
             input1: {
+                input_type: "select",
                 type: "text",
-                size: 30,
-                name: "email"
+                maxlength: 30,
+                name: "Email",
+                options: {
+                    test1: "Test1",
+                    test2: "Test2",
+                    test3: "Test3"
+                }
+            },
+            input2: {
+                input_type: "input",
+                type: "radio",
+                name: "pet",
+                options: {
+                    dog: "Dog",
+                    cat: "Cat",
+                    hamster: "Hamster"
+                }
             }
         }
     };
